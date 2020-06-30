c1c762fcf2e477bff23b0a4f6350299d1adfbbaa
Jacob Xie <jacobbishopxy@gmail.com>
2020-06-30 10:05:44 +0800

    MNT: update docker-app-* -- specify project name when calling docker-compose

--- docker-app-nodejs/docker-app/start.sh
+++ docker-app-nodejs/docker-app/start.sh
@@ -2,9 +2,10 @@
 # author: Jacob Bishop
 
 source ../resources/docker.env
+export COMPOSE_PROJECT_NAME=${COMPOSE_PROJECT_NAME}
 export APP_IMAGE_NAME=${APP_IMAGE_NAME}
 export APP_IMAGE_VERSION=${APP_IMAGE_VERSION}
 export CONTAINER_NAME=${CONTAINER_NAME}
 export APP_PORT=${APP_PORT}
 docker-compose down
-docker-compose up -d
+docker-compose -p ${COMPOSE_PROJECT_NAME} up -d
