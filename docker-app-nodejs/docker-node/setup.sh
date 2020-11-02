4261bb40c56d0a2bba006e25ee53174481055eea
Jacob Xie <jacobbishopxy@gmail.com>
2020-11-02 10:33:39 +0800

    MNT: update docker-app-nodejs

--- docker-app-nodejs/docker-node/setup.sh
+++ docker-app-nodejs/docker-node/setup.sh
@@ -2,6 +2,6 @@
 # author: Jacob Bishop
 
 source ../resources/docker.env
-docker pull "${FUND_IMAGE}":"${FUND_IMAGE_VERSION}"
+docker pull "${NODE_IMAGE}":"${NODE_IMAGE_VERSION}"
 docker image tag \
-  "${FUND_IMAGE}":"${FUND_IMAGE_VERSION}" "${BASE_IMAGE_NAME}":"${BASE_IMAGE_VERSION}"
+  "${NODE_IMAGE}":"${NODE_IMAGE_VERSION}" "${FUND_IMAGE}":"${FUND_IMAGE_VERSION}"
